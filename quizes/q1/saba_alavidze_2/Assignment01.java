6af4412842840113d87853bc64035c3f691d8fd6
1sunsula1 <162930610+1sunsula1@users.noreply.github.com>
2024-03-11 11:14:01 +0400

    Update Assignment01.java

--- quizes/q1/saba_alavidze_2/Assignment01.java
+++ quizes/q1/saba_alavidze_2/Assignment01.java
@@ -1,16 +1,11 @@
 package quizes.q1.saba_alavidze_2;
 
-
-/**
- * Correct formatting, naming conventions and the code itself
- * 1 point
- */
 public class Assignment01 {
     public static void main(String[] args) {
 
         int A = 17;
         int B = 18;
 
-        System.out.println("The sum of the numbers is" + A + B);
+        System.out.println("The sum of the numbers is " + (A + B));
     }
 }
