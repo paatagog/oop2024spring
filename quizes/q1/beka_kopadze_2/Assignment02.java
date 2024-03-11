148786c1809617bfc63c3a3d0f034915d659d9a7
beqa-kopadze <82507935+beqa-kopadze@users.noreply.github.com>
2024-03-11 11:59:11 +0400

    Update Assignment02.java

--- quizes/q1/beka_kopadze_2/Assignment02.java
+++ quizes/q1/beka_kopadze_2/Assignment02.java
@@ -1,12 +1,22 @@
 package quizes.q1.beka_kopadze_2;
+package quizes.q1.beka_kopadze_2.pencil;
+package quizes.qi.beka_kopadze_2.tablet
 
+class HappyGirl(){
+    
+}
 
+class HappyBoy(){
+    
+}
 /**
  * Find two packages inside this package.
  * Create classes A and B inside first and the second package accordingly
  * 1 point
 */
 public class Assignment02 {
+    HappyGirl happyGirl = new HappyGirl();
+    HappyBoy happyBoy = new HappyBoy();
     public static void main(String[] args) {
         System.out.println("This text is just for example");
     }
