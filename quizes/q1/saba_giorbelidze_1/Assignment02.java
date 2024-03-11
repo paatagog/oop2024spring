4accd6ef0b8b04264457acea5be081dc9c3412df
SaboOoOoOoO <152912335+SaboOoOoOoO@users.noreply.github.com>
2024-03-11 11:57:53 +0400

    Update Assignment02.java

--- quizes/q1/saba_giorbelidze_1/Assignment02.java
+++ quizes/q1/saba_giorbelidze_1/Assignment02.java
@@ -1,5 +1,15 @@
 package quizes.q1.saba_giorbelidze_1;
+package quizes.q1.saba_giorbelidze_1.guitar;
+package quizes.q1.saba_giorbelidze_1.hammer;
 
+    class HappyBoy(){
+        
+    } 
+
+    class HappyGirl(){
+
+        
+    }
 
 /**
  * Find two packages inside this package.
@@ -7,6 +17,8 @@ package quizes.q1.saba_giorbelidze_1;
  * 1 point
 */
 public class Assignment02 {
+    HappyGirl happygirl = new HappyGirl();
+    HappyBoy happyboy = new HappyBoy();
     public static void main(String[] args) {
         System.out.println("This text is just for example");
     }
