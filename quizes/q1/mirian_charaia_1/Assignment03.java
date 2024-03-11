1ca4cee5097ae6d0f0b130b3c48bc408a9ca9b6a
M2I0R0O5 <149790660+M2I0R0O5@users.noreply.github.com>
2024-03-11 12:44:08 +0400

    Update Assignment03.java

--- quizes/q1/mirian_charaia_1/Assignment03.java
+++ quizes/q1/mirian_charaia_1/Assignment03.java
@@ -6,6 +6,10 @@ package quizes.q1.mirian_charaia_1;
  * The file should be found yourself without java code.
  * 0.5 points
  */
-public class Assignment03 {
+package quizes.q1.mirian_charaia_1.fruit/guitar/paper;
 
+public class Assignment03 {
+    public static void main(String[] args) {
+        System.out.println("path? I guess");
+    }
 }
