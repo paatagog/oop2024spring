47334f4864e083155187a8893853deefb1db4a33
SaboOoOoOoO <152912335+SaboOoOoOoO@users.noreply.github.com>
2024-03-11 11:43:35 +0400

    Update Assignment01.java

--- quizes/q1/saba_giorbelidze_1/Assignment01.java
+++ quizes/q1/saba_giorbelidze_1/Assignment01.java
@@ -8,9 +8,9 @@ package quizes.q1.saba_giorbelidze_1;
 public class Assignment01 {
     public static void main(String[] args) {
 
-        int A = 17;
-        int B = 18;
+        int a = 17;
+        int b = 18;
 
-        System.out.println("The sum of the numbers is" + A + B);
+        System.out.println("The sum of the numbers is " + (a + b) );
     }
 }
